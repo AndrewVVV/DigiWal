1d7c92ff7ed7a7b5db1005cb0eae33c00cf27980
GTO90 <gto90@protonmail.com>
2017-12-10 16:02:02 -0600

    Updated app extension bundle identifiers.

--- breadwallet/src/UserDefaultsUpdater.swift
+++ breadwallet/src/UserDefaultsUpdater.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 private enum AppGroup {
-    static let id = "group.org.voisine.breadwallet"
+    static let id = "group.io.digibyte.digibytewallet"
     static let requestDataKey = "kBRSharedContainerDataWalletRequestDataKey"
     static let receiveAddressKey = "kBRSharedContainerDataWalletReceiveAddressKey"
 }
