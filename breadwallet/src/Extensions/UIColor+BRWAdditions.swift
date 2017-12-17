e2b290f00b3150cec22779b7a4a81f5cf766d951
Datsun240 <datsun240z@protonmail.com>
2017-12-17 13:19:43 -0500

    DigiByte Assets
    * Updated Import/Create Wallet prompt colors
    
    Signed-off-by: Datsun240 <datsun240z@protonmail.com>

--- breadwallet/src/Extensions/UIColor+BRWAdditions.swift
+++ breadwallet/src/Extensions/UIColor+BRWAdditions.swift
@@ -97,7 +97,7 @@ extension UIColor {
     }
 
     static var blue: UIColor {
-        return UIColor(red: 76.0/255.0, green: 152.0/255.0, blue: 252.0/255.0, alpha: 1.0)
+        return UIColor(red: 0.0/255.0, green: 102.0/255.0, blue: 204.0/255.0, alpha: 1.0)
     }
 
     static var whiteTint: UIColor {
