f3076a3fda46dcb42cf2baf18a7a55bd656f9316
Datsun240 <datsun240z@protonmail.com>
2017-12-17 12:22:23 -0500

    DigiByte Assets
    * Adjusted gradient used throughout the app
    
    Signed-off-by: Datsun240 <datsun240z@protonmail.com>

--- breadwallet/src/Extensions/UIColor+BRWAdditions.swift
+++ breadwallet/src/Extensions/UIColor+BRWAdditions.swift
@@ -41,11 +41,11 @@ extension UIColor {
 
     // MARK: Gradient
     static var gradientStart: UIColor {
-        return UIColor(red: 247.0/255.0, green: 164.0/255.0, blue: 69.0/255.0, alpha: 1.0)
+        return UIColor(red: 0.0/255.0, green: 35.0/255.0, blue: 82.0/255.0, alpha: 1.0)
     }
 
     static var gradientEnd: UIColor {
-        return UIColor(red: 252.0/255.0, green: 83.0/255.0, blue: 148.0/255.0, alpha: 1.0)
+        return UIColor(red: 0.0/255.0, green: 102.0/255.0, blue: 204.0/255.0, alpha: 1.0)
     }
 
     static var offWhite: UIColor {
