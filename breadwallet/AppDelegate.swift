3aac67692dfb96fe3a97f39dfdcd3197dd3df91e
GTO90 <gto90@protonmail.com>
2018-02-18 22:03:55 -0600

    Updated app delegate

--- breadwallet/AppDelegate.swift
+++ breadwallet/AppDelegate.swift
@@ -35,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let applicationController = ApplicationController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        Buglife.shared().start(withAPIKey: "QHo8wnuboLCXBrc9WZhumQtt")
+		Buglife.shared().start(withAPIKey: "") // TODO: Replace me with the BugLife API Key
         let appearance = Buglife.shared().appearance
         appearance.tintColor = .white
         appearance.barTintColor = .blueGradientEnd
